b42ffc543438c250ed269d2f5b5029f890cfad2f
Rob Winch <rwinch@users.noreply.github.com>
2018-03-05 17:26:02 -0600

    SimpleAuthenticationWebFilter as simple as possible
    Still reproduces the issue

--- src/test/java/sample/EnableWebFluxSecurityTests.java
+++ src/test/java/sample/EnableWebFluxSecurityTests.java
@@ -17,76 +17,34 @@
 package sample;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Scope;
-import org.springframework.core.ReactiveAdapterRegistry;
-import org.springframework.core.annotation.Order;
 import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.core.io.buffer.DefaultDataBufferFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.security.authentication.ReactiveAuthenticationManager;
-import org.springframework.security.authentication.TestingAuthenticationToken;
-import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authorization.AuthenticatedReactiveAuthorizationManager;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.context.ReactiveSecurityContextHolder;
 import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextImpl;
-import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
-import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.reactive.result.method.annotation.AuthenticationPrincipalArgumentResolver;
-import org.springframework.security.web.reactive.result.view.CsrfRequestDataValueProcessor;
 import org.springframework.security.web.server.MatcherSecurityWebFilterChain;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.ServerHttpBasicAuthenticationConverter;
 import org.springframework.security.web.server.WebFilterChainProxy;
-import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
-import org.springframework.security.web.server.authentication.HttpBasicServerAuthenticationEntryPoint;
-import org.springframework.security.web.server.authentication.ServerAuthenticationEntryPointFailureHandler;
 import org.springframework.security.web.server.authorization.AuthorizationWebFilter;
 import org.springframework.security.web.server.authorization.ExceptionTranslationWebFilter;
-import org.springframework.security.web.server.context.WebSessionServerSecurityContextRepository;
 import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatcher;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.reactive.server.FluxExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.config.WebFluxConfigurer;
-import org.springframework.web.reactive.result.method.annotation.ArgumentResolverConfigurer;
-import org.springframework.web.reactive.result.view.AbstractView;
 import org.springframework.web.server.WebFilter;
 import reactor.core.Scannable;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Hooks;
 import reactor.core.publisher.Mono;
-import reactor.core.scheduler.Schedulers;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
-import java.util.List;
-import java.util.regex.MatchResult;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.security.config.web.server.ServerHttpSecurity.http;
 import static org.springframework.web.reactive.function.client.ExchangeFilterFunctions.Credentials.basicAuthenticationCredentials;
 import static org.springframework.web.reactive.function.client.ExchangeFilterFunctions.basicAuthentication;
 
@@ -120,12 +78,14 @@ public class EnableWebFluxSecurityTests {
 		Logger logger = LoggerFactory.getLogger(getClass());
 		WebTestClient client = WebTestClient
 				.bindToController(new Http200RestController())
-				.webFilter(this.springSecurityFilterChain,
+				.webFilter(
+			this.springSecurityFilterChain,
 			(exchange, chain) ->
 				Flux.from(sub -> {
-					Scannable.from(sub).actuals().map(Scannable::toString).forEach(logger::info);
+					Scannable.from(sub).actuals().map(Scannable::name).forEach(logger::info);
 					sub.onComplete();
-				}).then(ReactiveSecurityContextHolder.getContext())
+				})
+				.then(ReactiveSecurityContextHolder.getContext())
 					.map(SecurityContext::getAuthentication)
 					.flatMap( principal -> exchange.getResponse()
 						.writeWith(Mono.just(toDataBuffer(principal.getName()))))
@@ -144,9 +104,7 @@ public class EnableWebFluxSecurityTests {
 	}
 
 	public WebFilter authenticationFilter() {
-		SimpleAuthenticationWebFilter authenticationFilter = new SimpleAuthenticationWebFilter(authenticationManager());
-		authenticationFilter.setAuthenticationConverter(new ServerHttpBasicAuthenticationConverter());
-		return authenticationFilter;
+		return new SimpleAuthenticationWebFilter();
 	}
 
 	private WebFilter exceptionTranslation() {
@@ -161,13 +119,6 @@ public class EnableWebFluxSecurityTests {
 		MatcherSecurityWebFilterChain chain = new MatcherSecurityWebFilterChain(e -> ServerWebExchangeMatcher.MatchResult.match(),
 				Arrays.asList(authenticationFilter(), exceptionTranslation(), authorizationFilter()));
 		return new WebFilterChainProxy(chain);
-	}
-
-	private ReactiveAuthenticationManager authenticationManager() {
-		return t -> Mono.just(t)
-				.publishOn(Schedulers.parallel())
-				.map( a -> new UsernamePasswordAuthenticationToken(a.getPrincipal(), a.getCredentials(),
-						AuthorityUtils.createAuthorityList("ROLE_USER")));
 	}
 
 	private static DataBuffer toDataBuffer(String body) {
--- src/test/java/sample/SimpleAuthenticationWebFilter.java
+++ src/test/java/sample/SimpleAuthenticationWebFilter.java
@@ -1,28 +1,15 @@
 package sample;
 
-import org.springframework.security.authentication.ReactiveAuthenticationManager;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.context.ReactiveSecurityContextHolder;
 import org.springframework.security.core.context.SecurityContextImpl;
-import org.springframework.security.web.server.ServerHttpBasicAuthenticationConverter;
-import org.springframework.security.web.server.WebFilterExchange;
-import org.springframework.security.web.server.authentication.HttpBasicServerAuthenticationEntryPoint;
-import org.springframework.security.web.server.authentication.ServerAuthenticationEntryPointFailureHandler;
-import org.springframework.security.web.server.authentication.ServerAuthenticationFailureHandler;
-import org.springframework.security.web.server.authentication.ServerAuthenticationSuccessHandler;
-import org.springframework.security.web.server.authentication.WebFilterChainServerAuthenticationSuccessHandler;
-import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
-import org.springframework.security.web.server.context.ServerSecurityContextRepository;
-import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatcher;
-import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers;
-import org.springframework.util.Assert;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
-
-import java.util.function.Function;
+import reactor.core.scheduler.Schedulers;
 
 /**
  * @author Rob Winch
@@ -30,40 +17,21 @@ import java.util.function.Function;
  */
 public class SimpleAuthenticationWebFilter implements WebFilter {
 
-	private final ReactiveAuthenticationManager authenticationManager;
-
-	private ServerAuthenticationSuccessHandler authenticationSuccessHandler = new WebFilterChainServerAuthenticationSuccessHandler();
-
-	private Function<ServerWebExchange, Mono<Authentication>> authenticationConverter = new ServerHttpBasicAuthenticationConverter();
-
-	public SimpleAuthenticationWebFilter(ReactiveAuthenticationManager authenticationManager) {
-		Assert.notNull(authenticationManager, "authenticationManager cannot be null");
-		this.authenticationManager = authenticationManager;
-	}
-
 	@Override
 	public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
-		return this.authenticationConverter.apply(exchange)
+		return Mono.just(exchange)
+				.publishOn(Schedulers.parallel())
+				.filter(e -> e.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION))
 				.switchIfEmpty(chain.filter(exchange).then(Mono.empty()))
-				.flatMap( token -> authenticate(exchange, chain, token));
+				.flatMap( token -> authenticate(exchange, chain));
 	}
 
 	private Mono<Void> authenticate(ServerWebExchange exchange,
-			WebFilterChain chain, Authentication token) {
-		WebFilterExchange webFilterExchange = new WebFilterExchange(exchange, chain);
-		return this.authenticationManager.authenticate(token)
-				.flatMap(authentication -> onAuthenticationSuccess(authentication, webFilterExchange));
-	}
-
-	private Mono<Void> onAuthenticationSuccess(Authentication authentication, WebFilterExchange webFilterExchange) {
-		ServerWebExchange exchange = webFilterExchange.getExchange();
+			WebFilterChain chain) {
 		SecurityContextImpl securityContext = new SecurityContextImpl();
-		securityContext.setAuthentication(authentication);
-		return this.authenticationSuccessHandler.onAuthenticationSuccess(webFilterExchange, authentication)
+		securityContext.setAuthentication(new UsernamePasswordAuthenticationToken("user", "password",
+				AuthorityUtils.createAuthorityList("ROLE_USER")));
+		return chain.filter(exchange)
 				.subscriberContext(ReactiveSecurityContextHolder.withSecurityContext(Mono.just(securityContext)));
-	}
-
-	public void setAuthenticationConverter(Function<ServerWebExchange, Mono<Authentication>> authenticationConverter) {
-		this.authenticationConverter = authenticationConverter;
 	}
 }
